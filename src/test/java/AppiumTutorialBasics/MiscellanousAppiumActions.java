82c5e1c412a419807900913d9a016d889b28225b
Mohit <mohitethhack@gmail.com>
2024-04-11 20:48:08 +0530

    Further Test Cases

--- src/test/java/AppiumTutorialBasics/MiscellanousAppiumActions.java
+++ src/test/java/AppiumTutorialBasics/MiscellanousAppiumActions.java
@@ -1,8 +1,14 @@
 package AppiumTutorialBasics;
 
+import com.google.common.collect.ImmutableMap;
 import io.appium.java_client.AppiumBy;
+import io.appium.java_client.android.Activity;
+import io.appium.java_client.android.nativekey.AndroidKey;
+import io.appium.java_client.android.nativekey.KeyEvent;
 import org.openqa.selenium.By;
 import org.openqa.selenium.DeviceRotation;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.remote.RemoteWebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
@@ -12,8 +18,16 @@ public class MiscellanousAppiumActions extends BaseTest{
     @Test
     public void Miscellaneous(){
 
-        driver.findElement(AppiumBy.accessibilityId("Preference")).click();
-        driver.findElement(By.xpath("//android.widget.TextView[@content-desc='3. Preference dependencies']")).click();
+        //App Package & App Activity(To Directly Open a Page of App //command to get package and activity: adb shell dumpsys window | find "mCurrentFocus"
+        Activity activity = new Activity("io.appium.android.apis", "io.appium.android.apis.preference.PreferenceDependencies");
+        //driver.startActivity(activity); // No More in Appium, Depcriated
+
+        ((JavascriptExecutor) driver).executeScript("mobile: startActivity", ImmutableMap.of(
+                "intent", "io.appium.android.apis/io.appium.android.apis.preference.PreferenceDependencies"
+        ));
+
+        //driver.findElement(AppiumBy.accessibilityId("Preference")).click();
+        //driver.findElement(By.xpath("//android.widget.TextView[@content-desc='3. Preference dependencies']")).click();
         driver.findElement(By.id("android:id/checkbox")).click();
 
         DeviceRotation landScape = new DeviceRotation(0,0,90); // To Rotate the Device Screen to Landscape
@@ -22,8 +36,12 @@ public class MiscellanousAppiumActions extends BaseTest{
         driver.findElement(By.xpath("(//android.widget.RelativeLayout)[2]")).click();
         String alertTitle = driver.findElement(By.id("android:id/alertTitle")).getText();
         Assert.assertEquals(alertTitle, "WiFi settings");
-        driver.findElement(By.id("android:id/edit")).sendKeys("MohitWifi");
+        driver.setClipboardText("Rahul Wifi"); // to copy text to clipboard
+        driver.findElement(By.id("android:id/edit")).sendKeys(driver.getClipboardText()); // to retrieve text from clipboard
+        driver.pressKey(new KeyEvent(AndroidKey.ENTER));  // To Send any key press to app
         driver.findElements(By.className("android.widget.Button")).get(1).click();
+        driver.pressKey(new KeyEvent(AndroidKey.BACK));  // To Send any key to app
+        driver.pressKey(new KeyEvent(AndroidKey.HOME)); // To Send any key to app
 
     }
 
